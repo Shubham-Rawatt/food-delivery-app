d4b5dcb5d6c71e1bb0810b46cd257cf504631d43
Shubham-Rawat9 <shubhamrawat97614@gmail.com>
2025-11-01 13:01:37 +0530

    folder create

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+     <h2 className="text-center text-5xl mt-100 text-red-500">Food delivery app </h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Nav from '../../components/Navbar/nav'
+
+
+const Home = () => {
+  return (
+
+        <div>
+            <Nav/>
+
+        </div>
+)
+}
+
+export default Home
