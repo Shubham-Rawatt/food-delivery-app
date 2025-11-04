e8e24ccfce1546c8c2e6bfb3ce6749dbc1bfd69c
Shubham-Rawat9 <shubhamrawat97614@gmail.com>
2025-11-04 13:50:19 +0530

    update componets

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from '../../components/Navbar/nav'
 import  Categories  from '../../Category'
 import Card from '../../components/CardData/card'
@@ -6,24 +6,37 @@ import { food_items } from '../../Food'
 
 
 const Home = () => {
+
+  const [category , setCategory] = useState(food_items)
+
+  const showMenu =(categoryName) => {
+    if (categoryName === 'All') {
+        setCategory(food_items)
+    } else{
+      const newList = food_items.filter( 
+        (item) =>item.food_category === categoryName )
+      setCategory(newList)
+    }
+  }
+  
   return (
 <>
         <div className='border-t-slate-300 w-full min-h-screen'>
             <Nav/>
            <div className="flex flex-wrap justify-center items-center w-full gap-6">
             {
-            Categories.map((category) =>(
-              <div key={category.id} className="w-32 h-32 flex flex-col items-center justify-center p-3 text-lg text-gray-500 rounded-lg shadow-xl hover:bg-green-200 cursor-pointer transition-all">
-              {category.image}
-              {category.name}
+            Categories.map((cat) =>(
+              <div key={cat.id} className="w-32 h-32 flex flex-col items-center justify-center p-3 text-lg text-gray-500 rounded-lg shadow-xl hover:bg-green-200 cursor-pointer transition-all" onClick={() => showMenu(cat.name)}>
+              {cat.image}
+              {cat.name}
               </div>
             ))
             }
           </div> 
 
           <div className='w-full flex flex-wrap gap-5 px-4 justify-center items-center pt-5 pb-5'>
-            {food_items.map( (item) =>
-            <Card key={item.id} name={item.food_name} image={item.food_image} price={item.price} id={item.id} type={item.food_type}/>
+            {category.map( (item) =>
+            <Card key={item.id} name={item.food_name} image={item.food_image} price={item.price} id={item.id} type={item.food_type} />
             )}
           </div>
 
